2d1fdfcca4d5e0e20b491ef5255cdc4c89feb196
mjttong <145860909+mjttong@users.noreply.github.com>
2026-07-03 00:42:28 +0900

    chore: 환경별 프로파일 설정 분리

--- src/test/kotlin/com/brifo/server/ServerApplicationTests.kt
+++ src/test/kotlin/com/brifo/server/ServerApplicationTests.kt
@@ -3,8 +3,10 @@ package com.brifo.server
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
 
 @Import(TestcontainersConfiguration::class)
+@ActiveProfiles("test")
 @SpringBootTest
 class ServerApplicationTests {
 
