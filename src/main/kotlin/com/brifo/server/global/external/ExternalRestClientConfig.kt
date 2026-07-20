4a8d62b861c5395a98407f6e916613bc01a58ff8
NaHyeon647 <forzeu@sookmyung.ac.kr>
2026-07-20 22:56:42 +0900

    refactor: KIS 현재가 조회용 RestClient 설정 추가

---
+++ src/main/kotlin/com/brifo/server/global/external/ExternalRestClientConfig.kt
@@ -0,0 +1,31 @@
+package com.brifo.server.global.external
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.web.client.RestClient
+
+@Configuration
+class ExternalRestClientConfig {
+    @Bean("kisCurrentPriceRestClient")
+    fun kisCurrentPriceRestClient(
+        @Value("\${external.kis.base-url}")
+        baseUrl: String,
+    ): RestClient {
+        val policy =
+            ExternalApiCallPolicy.KIS_CURRENT_PRICE
+
+        val requestFactory =
+            SimpleClientHttpRequestFactory().apply {
+                setConnectTimeout(policy.timeout)
+                setReadTimeout(policy.timeout)
+            }
+
+        return RestClient
+            .builder()
+            .baseUrl(baseUrl)
+            .requestFactory(requestFactory)
+            .build()
+    }
+}
