c76e2b9863608590616c8dd9ff352b475a2c98bd
mjttong <mjttong@gmail.com>
2026-07-23 04:43:36 +0900

    feat: 뱃지 조회 API 구성

--- src/main/kotlin/com/brifo/server/badge/repository/BadgeRepository.kt
+++ src/main/kotlin/com/brifo/server/badge/repository/BadgeRepository.kt
@@ -2,8 +2,7 @@ package com.brifo.server.badge.repository
 
 import com.brifo.server.badge.entity.Badge
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
 
-interface BadgeRepository : JpaRepository<Badge, Long> {
-    fun findByPublicId(publicId: UUID): Badge?
+interface BadgeRepository : JpaRepository<Badge, Long>, BadgeQueryRepository {
+    fun findByCode(code: String): Badge?
 }
