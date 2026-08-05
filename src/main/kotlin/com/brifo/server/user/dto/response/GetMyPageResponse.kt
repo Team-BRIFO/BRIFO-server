c4bb08e8a6639d2ffb9a9eef5dfa10c6f1db26ca
mjttong <mjttong@gmail.com>
2026-08-05 21:22:11 +0900

    chore: openAPI 제약 추가

--- src/main/kotlin/com/brifo/server/user/dto/response/GetMyPageResponse.kt
+++ src/main/kotlin/com/brifo/server/user/dto/response/GetMyPageResponse.kt
@@ -1,5 +1,6 @@
 package com.brifo.server.user.dto.response
 
+import io.swagger.v3.oas.annotations.media.ArraySchema
 import java.util.UUID
 
 data class GetMyPageResponse(
@@ -11,6 +12,7 @@ data class GetMyPageResponse(
     val totalDecision: Int,
     val consecutiveDays: Int,
     val learnedTermCount: Int,
+    @field:ArraySchema(minItems = 1, maxItems = 3)
     val stocks: List<MyPageStock>,
 ) {
     data class MyPageStock(
--- src/main/kotlin/com/brifo/server/user/dto/response/GetUserProfileResponse.kt
+++ src/main/kotlin/com/brifo/server/user/dto/response/GetUserProfileResponse.kt
@@ -1,10 +1,12 @@
 package com.brifo.server.user.dto.response
 
+import io.swagger.v3.oas.annotations.media.ArraySchema
 import java.util.UUID
 
 data class GetUserProfileResponse(
     val nickname: String,
     val companyName: String,
+    @field:ArraySchema(minItems = 1, maxItems = 3)
     val stocks: List<UserProfileStock>,
 ) {
     data class UserProfileStock(
